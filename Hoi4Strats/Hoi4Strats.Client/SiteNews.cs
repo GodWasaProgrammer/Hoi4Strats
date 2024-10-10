e7fcf95e774879c82a6a87bfcaa796c5af908129
GodWasaProgrammer <bwnils@gmail.com>
2024-10-10 20:32:01 +0200

    Added basic insitenews component

---
+++ Hoi4Strats/Hoi4Strats.Client/SiteNews.cs
@@ -0,0 +1,9 @@
+﻿namespace Hoi4Strats.Client;
+
+public class SiteNews
+{
+    public string? Header { get; set; }
+    public string? NewsContent { get; set; }
+
+    public DateTime? DateOfNews { get; set; }
+}
