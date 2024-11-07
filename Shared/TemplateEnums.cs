c37413e43cdc885ea7df09fc76fbb43487f8e01e
GodWasaProgrammer <bwnils@gmail.com>
2024-11-07 04:45:28 +0100

    Fleshed out enums for the structure of CreateGuide WIP: CreateGuide

---
+++ Shared/TemplateEnums.cs
@@ -0,0 +1,43 @@
+﻿namespace SharedProj;
+
+public enum GuideTypes
+{
+    Select,
+    Land,
+    Sea,
+    Air,
+    Country,
+    AirTemplate,
+    LandTemplate,
+    NavalTemplate
+}
+
+public enum LandTemplate
+{
+    Infantry,
+    Tank,
+    MotorizedInfantry,
+    SpaceMarine,
+    Cavalry,
+    Garrison,
+}
+
+public enum AirTemplate
+{
+    Fighter,
+    CAS,
+    HeavyBomber,
+    Carrier,
+    Recon,
+    NavalBomber
+}
+
+public enum NavalTemplate
+{
+    Destroyer,
+    Cruiser,
+    Heavy,
+    SuperHeavy,
+    Carrier,
+    Submarine,
+}
