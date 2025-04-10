7c2629445ed5dc9a440592eafed503293a83c17f
GodWasaProgrammer <bwnils@gmail.com>
2025-04-10 17:21:25 +0200

    Made a DebugAuth Component to help with overviewing tokenization
    Modified minor details in Login and Home and the JS script to fetch the data

--- Hoi4Strats/Hoi4Strats.Client/wwwroot/authSync.js
+++ Hoi4Strats/Hoi4Strats.Client/wwwroot/authSync.js
@@ -1,9 +1,12 @@
-﻿window.authSync = {
+﻿console.log("authSync.js laddas...");
+window.authSync = {
     getCookie: (name) => {
+        console.log("Försöker hämta cookie:", name);
         const value = document.cookie.match(`(^|;)\\s*${name}\\s*=\\s*([^;]+)`);
         return value ? decodeURIComponent(value[2]) : null;
     },
     deleteCookie: (name) => {
+        console.log("Rensar cookie:", name);
         document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
     }
 };
