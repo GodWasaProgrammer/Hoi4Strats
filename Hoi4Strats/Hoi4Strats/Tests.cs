432b872cb06fead8762fbf24c89e6194fba12c6d
GodWasaProgrammer <72033235+GodWasaProgrammer@users.noreply.github.com>
2025-05-08 14:50:56 +0200

    changed iterative looping for menu

--- Hoi4Strats/Hoi4Strats/Tests.cs
+++ Hoi4Strats/Hoi4Strats/Tests.cs
@@ -1,37 +1,1 @@
-﻿using Hoi4Strats.Data;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.Data.SqlClient;
-
-namespace Hoi4Strats;
-
-public class Tests
-{
-    public static void TestConnection()
-    {
-        var connectionString = DButils.GetConnectionString();
-
-        using var connection = new SqlConnection(connectionString);
-        try
-        {
-            connection.Open();
-            Console.WriteLine("SQL Connection test Success!");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Houston, we got a SQL problem: {ex.Message}");
-        }
-    }
-
-    public static async Task EnsureRolesCreated(RoleManager<IdentityRole> roleManager)
-    {
-        var roles = new[] { "Admin", "User", "Editor", "Moderator", "GuideAdmin", "ForumAdmin" };
-
-        foreach (var role in roles)
-        {
-            if (!await roleManager.RoleExistsAsync(role))
-            {
-                await roleManager.CreateAsync(new IdentityRole(role));
-            }
-        }
-    }
-}
+﻿using Hoi4Strats.Data;using Microsoft.AspNetCore.Identity;using Microsoft.Data.SqlClient;namespace Hoi4Strats;public class Tests{    public static void TestConnection()    {        var connectionString = DButils.GetConnectionString();        using var connection = new SqlConnection(connectionString);        try        {            connection.Open();            Console.WriteLine("SQL Connection test Success!");        }        catch (Exception ex)        {            Console.WriteLine($"Houston, we got a SQL problem: {ex.Message}");        }    }    public static async Task EnsureRolesCreated(RoleManager<IdentityRole> roleManager)    {        var roles = new[] { "Admin", "User", "Editor", "Moderator", "GuideAdmin", "ForumAdmin" };        foreach (var role in roles)        {            if (!await roleManager.RoleExistsAsync(role))            {                await roleManager.CreateAsync(new IdentityRole(role));            }        }    }}
